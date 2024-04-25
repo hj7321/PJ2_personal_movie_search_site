ffba485f9fa227ebb97f3abcdc54b68be8da10b3
hj7321 <oboki2001@naver.com>
2024-04-25 19:48:21 +0900

    영화 데이터 제목, 평점, 줄거리 불러오기 성공

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
+const $movieContainer = document.querySelector(".movie-container");
+
 // TMDB 오픈 API를 이용하여 인기영화 데이터 가져오기
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzUwZGE1MWJhNmVkMjdlYzlmMDZjNzkzNDdlZTQyNSIsInN1YiI6IjY2MjVkNDQ2ZTI5NWI0MDE2NDlhNTZmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LwKRpRWLt3lSImtcWOQiis4LAkBC1o-OtFFjJUZhW-s",
-  },
+const loadJson = async () => {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMzUwZGE1MWJhNmVkMjdlYzlmMDZjNzkzNDdlZTQyNSIsInN1YiI6IjY2MjVkNDQ2ZTI5NWI0MDE2NDlhNTZmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LwKRpRWLt3lSImtcWOQiis4LAkBC1o-OtFFjJUZhW-s",
+    },
+  };
+
+  let response = await fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    options
+  );
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error(response);
+  }
 };
 
-fetch(
-  "https://api.themoviedb.org/3/account/21227227/rated/movies?language=en-US&page=1&sort_by=created_at.asc",
-  options
-)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+// 영화 컨테이너 안의 영화 박스에 영화 정보 넣기
+const getData = async () => {
+  let res = await loadJson();
+  console.log(res.results);
+  res.results.forEach((movie) => {
+    let temp_html = `
+    <div class="movie-box">
+      <img src="${movie.poster_path}" alt="" />
+      <div class="movie-info-box">
+        <div class="title-score">
+          <p class="title">${movie.title}</p>
+          <p class="score">${movie.vote_average}</p>
+        </div>
+        <p>${movie.overview}</p>
+      </div>
+    </div>`;
+
+    $movieContainer.innerHTML += temp_html;
+  });
+};
+
+getData();
